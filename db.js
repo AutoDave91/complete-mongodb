cbd6bfe60de31a02bd825c58b2a1b2370ac6c44d
AutoDave91 <davidwnewman91@gmail.com>
2022-07-21 14:14:38 -0500

    security fix

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb');
 
 let dbConnection;
-let uri = 'mongodb+srv://David:testing123@cluster0.0kpp8.mongodb.net/?retryWrites=true&w=majority'
 
 module.exports = {
     connectToDb: (cb) => {
